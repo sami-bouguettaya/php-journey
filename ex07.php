c7295634fb5cae0327065f224bf5b3617016ab59
sami-bouguettaya <samibouguettaya66@gmail.com>
2024-11-07 14:04:06 +0100

    php html select

---
+++ ex07.php
@@ -0,0 +1,15 @@
+<html>
+
+<body>
+
+<font size="2" face="Arial">Le texte en HTML</font>
+<?php
+// le code PHP --------
+?>
+
+<!-- retour au code HTML -->
+<br>
+<font size="2" face="Arial"> Il est <?php echo 'Bonjour'; ?></font>
+</body>
+
+</html>
